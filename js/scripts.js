a8274f56ab1dda8550d234bca276e50f050fda53
mi-kts <kuts.kutsenko@gmail.com>
2023-02-23 16:07:13 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+document.addEventListener("mousemove", parallax);
+
+function parallax(e) {
+    document.querySelectorAll(".object").forEach(function (move) {
+
+        const moving_value = move.getAttribute("data-value");
+        const x = (e.clientX * moving_value) / 250;
+        let y = (e.clientY * moving_value) / 250;
+
+        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+    });
+}
+
+new TypeIt(".simpleUsage", {
+    strings: "",
+    speed: 50,
+    waitUntilVisible: true,
+}).go();
+
+$(document).ready(function () {
+    //sticky navbar
+    window.onscroll = () => {
+        let header = document.querySelector('header');
+
+        if (window.pageYOffset > 0) {
+            header.classList.add("sticky")
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
+});
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (event) {
+    event.target // newly activated tab
+    event.relatedTarget // previous active tab
+})
+
